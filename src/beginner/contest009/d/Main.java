cf5a3703e0545c2c15e378b3c2c49c22e943b3ec
sckm <scal.che@gmail.com>
2014-05-31 17:20:14 +0900

    AtCoder Beginner 009 Problem D

--- src/beginner/contest009/d/Main.java
+++ src/beginner/contest009/d/Main.java
@@ -1,6 +1,7 @@
 package beginner.contest009.d;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -12,27 +13,55 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int k = Integer.parseInt(sc.next());
 		int m = Integer.parseInt(sc.next());
-		int[] a = new int[k];
-		int[] c = new int[k];
+		long[] a = new long[k];
+		long[] c = new long[k];
 		for(int i=0;i<k;i++)
-			a[i] = Integer.parseInt(sc.next());
+			a[i] = Long.parseLong(sc.next());
 		for(int i=0;i<k;i++)
-			c[i] = Integer.parseInt(sc.next());
+			c[i] = Long.parseLong(sc.next());
 		
 		solve(k, m , a, c);
 	}
 
-	public void solve(int k, int m, int[] a, int[] c) {
-		int res = 0;
-		ArrayList<Integer> list = new ArrayList<Integer>();
+	public void solve(int k, int m, long[] a, long[] c) {
+		long[][] mat = new long[k][k];
+		long[][] x = new long[k][1];
 		for(int i=0;i<k;i++){
-			list.add(c[i]&a[i]);
+			mat[0][i] = c[i];
+			x[k-i-1][0] = a[i];
 		}
-		System.out.println(m);
-		for(int i=k;i<m;i++){
-			
+		for(int i=1;i<k;i++)
+			mat[i][i-1] = -1;
+		
+		mat = matPow(mat, m-1, x);
+	
+		System.out.println(mat[k-1][0]);
+	}
+	
+	private long[][] matPow(long[][] mat, int n, long[][] a){
+		while(n>0){
+			if((n&1) == 1)
+				a = matXor(mat, a);
+
+			mat = matXor(mat, mat);
+			n = n>>1;
 		}
-		System.out.println(res);
+		
+		return a;
+	}
+	
+	private long[][] matXor(long[][] a, long[][] x){
+		long[][] c = new long[a.length][x[0].length];
+		
+		for(int i=0;i<a.length;i++){
+			for(int j=0;j<x.length;j++){
+				for(int k=0;k<x[0].length;k++){
+					c[i][k] = c[i][k] ^ (a[i][j] & x[j][k]);
+				}
+			}
+		}
+		
+		return c;
 	}
 
 }
